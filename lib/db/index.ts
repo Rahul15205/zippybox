5462b59eb5fe98c88affa87c5e6194ce5fb001da
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-06-14 11:18:12 +0530

    Add dotenv configuration and validation for DATABASE_URL; create signIn and signUp schemas

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,11 @@ import {drizzle} from 'drizzle-orm/neon-http'
 import {neon} from '@neondatabase/serverless'
 
 import * as schema from "./schema"
+import * as dotenv from "dotenv";
+dotenv.config({path: ".env.local"});
+if(!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not sent in .env.local");
+}
 
 const sql = neon(process.env.DATABASE_URL!)
 
